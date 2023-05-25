bf3477551157b56ec8917bcab6c53e5637b771d0
vegavictorj <108371628+vegavictorj@users.noreply.github.com>
2023-05-25 07:26:46 -0400

    Add pseudocode for game logic with display

--- script.js
+++ script.js
@@ -35,8 +35,38 @@ function playRound(playerSelection, computerSelection) {
 
 const result = document.querySelector(".result");
 const buttons = document.querySelectorAll("button");
+// select div playerScore
+// select div computerScore
+// select div finalResult
+// create variable for playerWins
+// create variable for playerLosses
+// create variable for playerDraws
+// create variable for computerWins
+// create variable for computerLosses
+// create variable for computerDraws
+// while playerWins < 5 || computerWins < 5
 buttons.forEach(button => button.addEventListener("click", () => {
     const playerSelection = button.textContent;
     const computerChoice = getComputerChoice();
     result.textContent = playRound(playerSelection, computerChoice);
+    // if result.textContent.slice(0,7) === "You Win"
+    //      playerWins++
+    //      computerLosses++
+    // playerScore.textContent = `Player Score: W-${playerWins} L-${playerLosses} D-${playerDraws}`
+    // computerScore.textContent = `Computer Score: W-${computerWins} L-${computerLosses} D-${computerDraws}`
+    // else if result.textContent.slice(0,8) === "You Lose"
+    //      playerLosses++
+    //      computerWins++
+    // playerScore.textContent = `Player Score: W-${playerWins} L-${playerLosses} D-${playerDraws}`
+    // computerScore.textContent = `Computer Score: W-${computerWins} L-${computerLosses} D-${computerDraws}`
+    // else 
+    //      playerDraws++
+    //      computerDraws++
+    // playerScore.textContent = `Player Score: W-${playerWins} L-${playerLosses} D-${playerDraws}`
+    // computerScore.textContent = `Computer Score: W-${computerWins} L-${computerLosses} D-${computerDraws}`
+
+    // if playerWins === 5
+    // finalResult.textContent = "Aye! Good job, you won!"
+    // else
+    // finalResult.textContent = "Ouff! we'll get them next time!"
 }));
